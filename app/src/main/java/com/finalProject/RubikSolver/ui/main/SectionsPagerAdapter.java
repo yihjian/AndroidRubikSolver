e2f356d99926421d954a5f2097c3abde40849439
yihjian <yihongj2@illinois.edu>
2019-11-19 15:41:41 -0600

    cleanup initial layout:

--- app/src/main/java/com/finalProject/RubikSolver/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/finalProject/RubikSolver/ui/main/SectionsPagerAdapter.java
@@ -30,7 +30,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
         switch (position) {
             case 0:
                 FragmentSolve solveTab = new FragmentSolve();
